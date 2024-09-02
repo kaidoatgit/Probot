b8889e5e0ac46f88436d27ec3f03ec8ab705b7ca
fma-7 <farhantrabalho@outlook.com>
2024-09-02 18:23:19 +0100

    Project Shared: Removed unnecessary property IsSuccess

--- Shared/Dtos/Metadata.cs
+++ Shared/Dtos/Metadata.cs
@@ -4,5 +4,4 @@ public class Metadata
 {
     public int StatusCode { get; set; }
     public string ErrorMessage { get; set; } = string.Empty;
-    public bool IsSuccess => StatusCode >= 200 && StatusCode < 300;
 }
