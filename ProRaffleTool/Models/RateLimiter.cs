3644872a4ce1379c2449515378566f77d381b15c
fma-7 <farhantrabalho@outlook.com>
2024-09-13 09:12:49 +0100

    Project ProRaffleTool: the waiting method for rate limit was commented out

--- ProRaffleTool/Models/RateLimiter.cs
+++ ProRaffleTool/Models/RateLimiter.cs
@@ -6,26 +6,18 @@ public class RateLimiter
 {
     private readonly SemaphoreSlim _semaphore = new(1, 1);
     private readonly TimeSpan _defaultDelay  = TimeSpan.FromSeconds(15);
-    private volatile bool _apiRequestWaiting = false;
-
-    // public async Task WaitAsync(TimeSpan? delay = null)
-    // {
-    //     var delayToUse = delay ?? _defaultDelay;
-    //     await _semaphore.WaitAsync();
-    //     try
-    //     {
-    //         await Task.Delay(delayToUse);
-    //     }
-    //     finally
-    //     {
-    //         _semaphore.Release();
-    //     }
-    // }
 
     public async Task WaitAsync(TimeSpan? delay = null)
     {
         var delayToUse = delay ?? _defaultDelay;
-        await _semaphore.WaitAsync(delayToUse);
-        _semaphore.Release();
+        await _semaphore.WaitAsync();
+        try
+        {
+            await Task.Delay(delayToUse);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 }
